e9d3edab043c7774de121f5e51404c18ea3930ad
z80 <bashkirov.sergey@gmail.com>
2016-01-03 10:03:40 +0300

    Filling motor control file.

--- firmware/lcu03/src/moto_ctrl.c
+++ firmware/lcu03/src/moto_ctrl.c
@@ -2,6 +2,25 @@
 #include "moto_ctrl.h"
 #include "hal.h"
 
+#define SLEEP_PORT GPIOA
+#define SLEEP_PAD  6
+
+#define RESET_PORT GPIOA
+#define RESET_PAD  7
+
+#define ENABLE_PORT GPIOB
+#define ENABLE_PAD  10
+
+#define DIR_0_PORT GPIOB
+#define DIR_0_PAD  11
+
+#define DIR_1_PORT GPIOA
+#define DIR_1_PAD  4
+
+#define HIGH_CURRENT_PORT GPIOB
+#define HIGH_CURRENT_PAD  13
+
+
 typedef struct
 {
 	int     pos;
@@ -53,6 +72,48 @@ static const EXTConfig extcfg = {
                 0)
 };
 
+#define PWM_FREQ 140625
+
+
+static void pwmMotor0( PWMDriver * pwmp );
+static void pwmMotor1( PWMDriver * pwmp );
+
+static PWMConfig pwmcfgMotor0 = {
+  PWM_FREQ,
+  PWM_FREQ, // Initial PWM period 1S.
+  pwmMotor0,
+  {
+   { PWM_OUTPUT_DISABLED, NULL },
+   { PWM_OUTPUT_DISABLED, NULL },
+   { PWM_OUTPUT_ACTIVE_HIGH, NULL },
+   { PWM_OUTPUT_DISABLED, NULL }
+  },
+  0,
+  0,
+#if STM32_PWM_USE_ADVANCED
+  0
+#endif
+};
+
+static PWMConfig pwmcfgMotor1 = {
+  PWM_FREQ,
+  PWM_FREQ, // Initial PWM period 1S.
+  pwmMotor1,
+  {
+   { PWM_OUTPUT_DISABLED, NULL },
+   { PWM_OUTPUT_ACTIVE_HIGH, NULL },
+   { PWM_OUTPUT_DISABLED, NULL },
+   { PWM_OUTPUT_DISABLED, NULL }
+  },
+  0,
+  0,
+#if STM32_PWM_USE_ADVANCED
+  0
+#endif
+};
+
+
+
 #define MOTOR_QUEUE_SZ 5
 
 typedef struct
@@ -87,9 +148,21 @@ void motorInit( void )
 
 	chIQInit( &motor_queue,   motor_queue_buffer, SWEEP_BUFFER_SZ, 0 );
 
-
 	// Initialize external interrupt input here.
 	extStart(&EXTD1, &extcfg);
+
+	// Init PWM for step control.
+	pwmStart( &PWMD3, &pwmcfgMotor0 );
+	palSetPadMode( GPIOB, 0, PAL_MODE_STM32_ALTERNATE_PUSHPULL );
+
+	pwmStart( &PWMD2, &pwmcfgMoto1 );
+	palSetPadMode( GPIOA, 1, PAL_MODE_STM32_ALTERNATE_PUSHPULL );
+
+	// Init GPIO ~sleep~, ~enable~, ~reset~, ~high_current~.
+	palClearPad( ENABLE_PORT, ENABLE_PAD );
+	palClearPad( SLEEP_PORT,  SLEEP_PAD );
+	palClearPad( RESET_PORT,  RESET_PAD );
+	palSetPad(  HIGH_CURRENT_PORT, HIGH_CURRENT_PAD );
 }
 
 
@@ -125,6 +198,17 @@ int motorPos( int index )
 {
 
 }
+
+static void pwmMotor0( PWMDriver * pwmp )
+{
+
+}
+
+static void pwmMotor1( PWMDriver * pwmp )
+{
+
+}
+
 
 
 
