a9e9f4499f7e0b90dbfaa04257cd798e99c83423
z80 <bashkirov.sergey@gmail.com>
2016-01-08 10:15:14 +0300

    Decreased even more shutter active period.

--- firmware/lcu03/src/shutter_ctrl.c
+++ firmware/lcu03/src/shutter_ctrl.c
@@ -6,7 +6,7 @@
 #define SH_PIN_0  0
 #define SH_PORT_1 GPIOC
 #define SH_PIN_1  15
-#define SH_SLEEP 200
+#define SH_SLEEP 50
 
 static WORKING_AREA( waShutter, 256 );
 static msg_t shutterThread( void *arg );
