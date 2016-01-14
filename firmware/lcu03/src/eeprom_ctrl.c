67d2264438db1a38ca7d90e266efb33fd227ae0d
unknown <z80@cortex-m3.(none)>
2016-01-13 17:00:30 -0800

    Started eeprom implementation.

---
+++ firmware/lcu03/src/eeprom_ctrl.c
@@ -0,0 +1,9 @@
+
+#include "eeprom_ctrl.h"
+#include "hal.h"
+
+void eepromInit( void )
+{
+
+}
+
---
+++ firmware/lcu03/src/eeprom_ctrl.h
@@ -0,0 +1,11 @@
+
+#ifndef __EEPROM_CTRL_H_
+#define __EEPROM_CTRL_H_
+
+#include "ch.h"
+
+void eepromInit( void );
+
+#endif
+
+
